8d3cac3ac189b8ef8dcc30b92fcab0744eba913f
r23 <r23>
2003-03-28 17:43:12 +0000

    misc update

--- html/html/install/index.php
+++ html/html/install/index.php
@@ -1,6 +1,6 @@
 <?php
 /* ----------------------------------------------------------------------
-   $Id: index.php,v 1.3 2003/03/28 02:54:52 r23 Exp $
+   $Id: index.php,v 1.4 2003/03/28 17:43:12 r23 Exp $
 
    OSIS WebPrinter for your Homepage
    http://www.osisnet.de
@@ -55,144 +55,95 @@
 
 /** initialize vars, and include all necessary files **/
 
-set_time_limit(0);
+ set_time_limit(0);
 
-define('ADODB_DIR', '../includes/adodb');
-require_once ('../includes/adodb/adodb.inc.php');
-require_once ('../includes/classes/osis_text_tool.php');
+ define('ADODB_DIR', '../includes/adodb');
+ require_once ('../includes/adodb/adodb.inc.php');
+ require_once ('../includes/classes/osis_text_tool.php');
 
-if (isset($alanguage)) {
-  $currentlang = $alanguage;
-}
+ if (isset($alanguage)) {
+   $currentlang = $alanguage;
+ }
 
-if (!isset($prefix)) {
-  include_once '../includes/config.php';
-  $prefix = $owconfig['prefix'];
-  $dbtype = $owconfig['dbtype'];
-  $dbhost = $owconfig['dbhost'];
-  $dbuname = $owconfig['dbuname'];
-  $dbpass = $owconfig['dbpass'];
-  $dbname = $owconfig['dbname'];
-  $system = $owconfig['system'];
-  $encoded = $owconfig['encoded'];   
-}
+ if (!isset($prefix)) {
+    include_once '../includes/config.php';
+    $prefix = $owconfig['prefix'];
+    $dbtype = $owconfig['dbtype'];
+    $dbhost = $owconfig['dbhost'];
+    $dbuname = $owconfig['dbuname'];
+    $dbpass = $owconfig['dbpass'];
+    $dbname = $owconfig['dbname'];
+    $system = $owconfig['system'];
+    $encoded = $owconfig['encoded'];   
+  }
 
-if (!empty($encoded)) {
-  $dbuname = base64_decode($dbuname);
-  $dbpass = base64_decode($dbpass);
-}
+  if (!empty($encoded)) {
+    $dbuname = base64_decode($dbuname);
+    $dbpass = base64_decode($dbpass);
+  }
 
-$owconfig['prefix'] = $prefix;
+  $owconfig['prefix'] = $prefix;
 
-include_once 'language.php'; 
+  include_once 'language.php'; 
 
-installer_get_language();
+  installer_get_language();
 
-include_once 'modify_config.php'; 
-include_once 'newinstall.php'; 
-include_once 'gui.php'; 
-include_once 'db.php'; 
-include_once 'check.php'; 
+  include_once 'modify_config.php'; 
+  include_once 'newinstall.php'; 
+  include_once 'gui.php'; 
+  include_once 'db.php'; 
+  include_once 'check.php'; 
 
-/** print the page header, include style sheets **/
-print_header();
+// header
+  include_once 'header.php';
 
 /*  This starts the switch statement that filters through the form options.
  * the @ is in front of $op to suppress error messages if $op is unset and E_ALL
  * is on
  */
-switch(@$op) {
-
-    case "CHM_check":
-         print_CHM_check();
-         break;
-
-    case "Submit":
-         print_submit();
-         break;
-
-    case "Change Info":
-         print_change_info();
-         break;
-
-    case "New Install":
-         print_new_install();
-         break;
-    
-    case "Start":
-         if(!isset($dbmake)) {
-            $dbmake = false;
-         }
-         make_db($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype, $dbmake);
-         print_start();
-         break;
-
-    case "Continue":
-         print_continue();
-         break;
-
-    Case "Set Login":
-         $dbconn = dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype);
-         input_data($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype, $aid, $name, $pwd, $repeatpwd, $email, $url);
-         update_config_php(true); // Scott - added
-         print_set_login();
-         break;
-
-    Case "Select Language":
-         print_select_language();
-         break;
-
-    Case "Set Language":
-         $currentlang = $alanguage;
-         print_default();
-         break;
-
-    Case "Finish":
-         print_finish();
-         break;
-
-    Case "Upgrade":
-         print_upgrade();
-         break;
-
-    Case "PHP-Nuke":
-         print_select_phowuke();
-         break;
-
-    Case "PostNuke":
-         print_select_postnuke();
-         break;
-
-    Case "MyPHPNuke":
-         print_select_myphowuke();
-         break;
-         
-/* Removed for release.  Needs to be updated
-
-    Case "Validate Language":
-         $dbconn = dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype);
-         language_update($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype);
-         break;
-
-    Case "Validate Tables":
-         $dbconn = dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype);
-         tables_update($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype);
-         break;
-
-    Case "Validate Sequence Tables":
-         $dbconn = dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype);
-         sequence_update($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype);
-         break; */
-        
-    case "Check":
-        do_check_php();
-        do_check_chmod();
-        break;
-
+ switch ($_POST['op']) {
+    case 'Finish':
+      print_finish();
+      break;
+    case 'Set Login':
+      $dbconn = dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype);
+      input_data($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype, $aid, $name, $pwd, $repeatpwd, $email, $url);
+      update_config_php(true); // Scott - added
+      print_set_login();
+      break;
+    case 'Continue':
+      print_continue();
+      break;
+    case 'Start':
+      if (!isset($dbmake)) {
+        $dbmake = false;
+      }
+      make_db($dbhost, $dbuname, $dbpass, $dbname, $prefix, $dbtype, $dbmake);
+      print_start();
+      break;
+    case 'New Install':
+      print_new_install();
+      break;
+    case 'Change Info':
+      print_change_info();
+      break;
+    case 'Submit':
+      print_submit();
+      break;
+    case 'CHM_check':
+      print_CHM_check();
+      break;
+    case 'Check':
+      do_check_php();
+      do_check_chmod();
+      break;
+    case 'Set Language':
+      $currentlang = $alanguage;
+      print_default();
+      break;       
     default:
-         print_select_language();
-         break;
-}
-
-print_footer();
+      print_select_language();
+      break;
+  }
+  include_once 'footer.php';
 ?>
