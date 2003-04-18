b5b614fc25f0d7c35add27bc139a94b066b60a3e
r23 <r23>
2003-04-18 22:52:09 +0000

    initial import

---
+++ html/html/includes/language/deu/define_language.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id: define_language.php,v 1.1 2003/04/18 22:52:09 r23 Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Sprache definieren');
+
+define('TEXT_FILE_DOES_NOT_EXIST', 'Datei nicht vorhanden.');
+
+define('ERROR_FILE_NOT_WRITEABLE', 'Fehler: Die Datei ist schreibgesch&uuml;tzt. Bitte korrigieren Sie die Zugriffsrechte f&uuml;r: %s');
+?>
