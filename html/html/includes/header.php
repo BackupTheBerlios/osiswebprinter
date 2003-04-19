27cf2f1cae1d8c25c8734ec3b296c17b916f9e9b
r23 <r23>
2003-04-19 21:33:53 +0000

    gnu

--- html/html/includes/header.php
+++ html/html/includes/header.php
@@ -1,14 +1,26 @@
 <?php
-/*
-  $Id: header.php,v 1.1 2003/04/19 21:32:40 r23 Exp $
+/* ----------------------------------------------------------------------
+   $Id: header.php,v 1.2 2003/04/19 21:33:53 r23 Exp $
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+   OSIS WebPrinter for your Homepage
+   http://www.osisnet.de
+   
+   Ralf Zschemisch
+   http://www.r23.de/
+   
+   Copyright (c) 2003 r23
+   ----------------------------------------------------------------------
+   Based on:
+   
+   File: header.php,v 1.19 2002/04/13 16:11:52 hpdl
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce
-
-  Released under the GNU General Public License
-*/
+   Copyright (c) 2002 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
 
   if ($messageStack->size > 0) {
     echo $messageStack->output();
