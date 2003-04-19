bb761657b6a4201e08160bb9327d9f9cf305942b
r23 <r23>
2003-04-19 09:03:02 +0000

    initial import

---
+++ html/html/includes/column_left.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id: column_left.php,v 1.1 2003/04/19 09:03:02 r23 Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  require(DIR_WS_BOXES . 'configuration.php');
+  require(DIR_WS_BOXES . 'catalog.php');
+  require(DIR_WS_BOXES . 'modules.php');
+  require(DIR_WS_BOXES . 'customers.php');
+  require(DIR_WS_BOXES . 'taxes.php');
+  require(DIR_WS_BOXES . 'localization.php');
+  require(DIR_WS_BOXES . 'reports.php');
+  require(DIR_WS_BOXES . 'tools.php');
+?>
