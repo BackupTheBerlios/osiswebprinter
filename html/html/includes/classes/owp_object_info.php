deffdb0079c72fbc62616c588dbafe59d5ba6a26
r23 <r23>
2003-04-23 06:30:23 +0000

    initial import

---
+++ html/html/includes/classes/owp_object_info.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id: owp_object_info.php,v 1.1 2003/04/23 06:30:23 r23 Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2001 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  class objectInfo {
+
+// class constructor
+    function objectInfo($object_array) {
+      reset($object_array);
+      while (list($key, $value) = each($object_array)) {
+        $this->$key = tep_db_prepare_input($value);
+      }
+    }
+  }
+?>
