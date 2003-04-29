dae1f289fc88623e4b8cf891f36c9cc5a40df8dd
r23 <r23>
2003-04-29 16:52:25 +0000

    initial import

---
+++ html/html/includes/language/deu/modules/newsletters/newsletter.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id: newsletter.php,v 1.1 2003/04/29 16:52:25 r23 Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('TEXT_COUNT_CUSTOMERS', 'Anzahl der Kunden, welche das Rundschreiben erhalten: %s');
+?>
